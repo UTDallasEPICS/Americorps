39ec25fe0a6930c17c35216c2a6a2519947a2b81
Kartik Singhal <singhalkartik72@gmail.com>
2020-10-29 15:04:55 +0530

    removing invalid import

--- Auth v1/lib/ui/views/home/home_view.dart
+++ Auth v1/lib/ui/views/home/home_view.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_web_firebase_google_auth/app/models/user.dart';
 import 'package:provider/provider.dart';
 import '../../../app/constants/strings.dart';
 import '../../../app/services/firebase_auth_service.dart';
