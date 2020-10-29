6a0c341db085d89eee2038f08410095411774b9d
Kartik Singhal <singhalkartik72@gmail.com>
2020-10-29 17:26:20 +0530

    Name of user
    Can use name of user now, anywhere expect in home_view. (and sign_in_view)

--- Auth v1/lib/ui/views/home/home_view.dart
+++ Auth v1/lib/ui/views/home/home_view.dart
@@ -1,13 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import '../time/time_view.dart';
 import 'package:provider/provider.dart';
 import '../../../app/constants/strings.dart';
 import '../../../app/services/firebase_auth_service.dart';
 
+String name = '';
+final FirebaseAuth _auth = FirebaseAuth.instance;
+getCurrentUserName() async {
+  final user = await _auth.currentUser();
+  if (user.isAnonymous) {
+    name = 'Guest';
+  } else {
+    name = user.displayName;
+  }
+}
+
 class HomeView extends StatelessWidget {
   const HomeView({Key key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    getCurrentUserName();
     return Scaffold(
       body: Center(
         child: Column(
--- Auth v1/lib/ui/views/time/time_view.dart
+++ Auth v1/lib/ui/views/time/time_view.dart
@@ -1,28 +1,33 @@
 import 'package:flutter/material.dart';
-import '../../../app/models/user.dart';
-import '../../../app/services/firebase_auth_service.dart';
+import '../home/home_view.dart';
+
+var timeOfDay = TimeOfDay.now();
 
 class TimeView extends StatelessWidget {
   TimeView({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+    //getCurrentUserName();
     return Scaffold(
         body: Center(
             child: Column(children: <Widget>[
       Padding(
           padding: const EdgeInsets.all(16),
           child: Text(
-            timeOfDay.hour.toString() +
+            'The time is now ' +
+                timeOfDay.hour.toString() +
                 ':' +
                 timeOfDay.minute.toString() +
-                '\n',
+                ', ' +
+                name,
             style: Theme.of(context).textTheme.headline4,
-          ))
+          )),
+      RaisedButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        child: const Text('Back'),
+      )
     ])));
   }
 }
-
-var timeOfDay = TimeOfDay.now();
-Stream<User> user = FirebaseAuthService().currentUser().asStream();
-var name = user.first.then((value) => value.displayName);
