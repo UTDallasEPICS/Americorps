629aa0770f730eacfe37093187e1baadec41556b
Kartik Singhal <singhalkartik72@gmail.com>
2020-11-10 03:05:13 +0530

    Update firebase_auth_service.dart

--- Auth v1/lib/app/services/firebase_auth_service.dart
+++ Auth v1/lib/app/services/firebase_auth_service.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import '../../ui/views/authentication/sign_in/sign_in_view.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
