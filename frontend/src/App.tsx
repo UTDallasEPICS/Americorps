ce8592ebbd9b5dd513c6bf9c06cdbb5eea928cc4
Ashleigh Trout <ajt190001@utdallas.edu>
2021-04-29 15:07:43 -0500

    Deleted navbar and routing to prepare for merge with Shiva's branch.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import NavBar from './components/admin-navbar';
 import AdminHome from './pages/admin-home';
 import Users from './pages/admin-users-list';
 import './components/AshStyle.css'
@@ -8,13 +7,7 @@ import './components/AshStyle.css'
 function App() {
   return (
     <div className="App">
-       <Router>
-        <NavBar />
-        <Switch>
-          <Route path="/" exact component={() => <AdminHome />} />
-          <Route path="/Users" exact component={() => <Users />} />
-        </Switch>
-      </Router>
+       
     </div>
   );
 }
