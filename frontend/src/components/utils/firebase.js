0e1842bbc1a23643408c10c72903a8caafe9d008
anizhou <annie.zhou.611@gmail.com>
2021-10-20 16:43:15 -0500

    firebase.js testing

--- frontend/src/components/utils/firebase.js
+++ frontend/src/components/utils/firebase.js
@@ -18,3 +18,16 @@ firebase.initializeApp(firebaseConfig);
 export const db = firebase.firestore();
 
 export default firebase;
+
+import { collection, addDoc } from "firebase/firestore"; 
+
+try {
+  const docRef = await addDoc(collection(db, "users"), {
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+  });
+  console.log("Document written with ID: ", docRef.id);
+} catch (e) {
+  console.error("Error adding document: ", e);
+}
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,7 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import firebase from "./components/utils/firebase";
+import { collection, addDoc } from "firebase/firestore"; 
 
+try {
+  const docRef = await addDoc(collection(db, "users"), {
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+  });
+  console.log("Document written with ID: ", docRef.id);
+} catch (e) {
+  console.error("Error adding document: ", e);
+}
 
 ReactDOM.render(
   <React.StrictMode>
