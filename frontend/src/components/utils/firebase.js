3fb512b33f03efebe222abd574847060e5e2dfdc
anizhou <annie.zhou.611@gmail.com>
2021-10-20 16:33:12 -0500

    Update firebase Config data

--- frontend/src/components/utils/firebase.js
+++ frontend/src/components/utils/firebase.js
@@ -4,15 +4,15 @@ import "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAv_Q0TIDOXCyOjb0guTSUbgYNC4_QiwZg",
-    authDomain: "epcs-americorps.firebaseapp.com",
-    databaseURL: "https://epcs-americorps-default-rtdb.firebaseio.com",
-    projectId: "epcs-americorps",
-    storageBucket: "epcs-americorps.appspot.com",
-    messagingSenderId: "639867939960",
-    appId: "1:639867939960:web:7ad284cd3c0a516fc1918d",
-    measurementId: "G-0L5474GXWT"
-  };
+  apiKey: "AIzaSyAv_Q0TIDOXCyOjb0guTSUbgYNC4_QiwZg",
+  authDomain: "epcs-americorps.firebaseapp.com",
+  databaseURL: "https://epcs-americorps-default-rtdb.firebaseio.com",
+  projectId: "epcs-americorps",
+  storageBucket: "epcs-americorps.appspot.com",
+  messagingSenderId: "639867939960",
+  appId: "1:639867939960:web:1e510c188fd5e37dc1918d",
+  measurementId: "G-ZM76V5BY80"
+};
 
 firebase.initializeApp(firebaseConfig);
 export const db = firebase.firestore();
