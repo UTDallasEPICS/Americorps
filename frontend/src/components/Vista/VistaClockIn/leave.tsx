2e1043c0147e8eb7b7006be0fe56ceeaf0934fae
Olimjon Nematov <macmini@Olimjons-Mac-mini.local>
2021-11-04 14:09:34 -0500

    added leaves

--- frontend/src/components/Vista/VistaClockIn/leave.tsx
+++ frontend/src/components/Vista/VistaClockIn/leave.tsx
@@ -1,9 +1,38 @@
+import { Grid } from "@material-ui/core";
+import { useState } from "react";
 import styles from "./styles.module.css"
 
 export default function Leave(){
+    const [leave, setLeave]= useState(0);
     return(
         <div className={styles.leavesDiv}>
-            <h1>Leaves</h1>
+            <form>
+                <label className={styles.leaveOps}>
+                    <input
+                        name="isGoing"
+                        type="radio"
+                        className={styles.radioIns}
+                        />
+                    <p>Emergency Leave</p>
+                </label>
+                <label className={styles.leaveOps}>
+                    <input
+                        name="numberOfGuests"
+                        type="radio"
+                        className={styles.radioIns}
+                        />
+                    <p>Personal Leave</p>
+                </label>
+                <label className={styles.leaveOps}>
+                    <input
+                        name="numberOfGuests"
+                        type="radio"
+                        className={styles.radioIns}
+                        />
+                    <p>Medical Leave</p>
+                </label>
+
+            </form>
         </div>
     );
 }
