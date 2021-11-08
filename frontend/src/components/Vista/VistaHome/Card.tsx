680d546f970313b4b6ea25852aaf8ac276806f88
Stephanie Li <listeph@gmail.com>
2021-11-07 21:27:37 -0600

    updated card

--- frontend/src/components/Vista/VistaHome/Card.tsx
+++ frontend/src/components/Vista/VistaHome/Card.tsx
@@ -1,35 +1,59 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import styles from './styles.module.css';
+// import styles from './styles.module.css'; take out?
+import { makeStyles } from "@material-ui/core/styles";
 
-export default function SimpleCard() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: theme.spacing(30),
+      height: theme.spacing(40)
+    }
+  },
+  blueCard: {
+    backgroundImage: "linear-gradient(to bottom, #047257 50%, #061532 50%)",
+    borderRadius: 25,
+    maxWidth: 300
+  },
+
+
+  textStyle1: {
+    color: "white",
+    padding: 50,
+    fontWeight: 600
+  },
+  textStyle2: {
+    color: "white"
+  }
+}));
+
+export default function Variants() {
+  const classes = useStyles();
+
   return (
-    <Card className={styles.root}>
-      <CardContent >
-        <div className={styles.main}>
-          <Typography className={styles.title} gutterBottom>
-            Here is your upcoming event:
-          </Typography>
-          <Typography variant="h5" component="h2">
-            12:33pm
-          </Typography>
-          <Typography className={styles.pos}>
-            on call
-          </Typography>
-        </div>
-        <div className={styles.events}>
-          <Typography className={styles.events}>
-            No upcoming events
-          </Typography>
-        </div>
-      </CardContent>
-      <CardActions className={styles.learnMore} >
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+    <div className={classes.root}>
+      <Card className={classes.blueCard}>
+        <CardContent>
+          <div className={classes.textStyle1}>
+            <Typography variant="h5" align="center">
+              12:06 PM
+            </Typography>
+
+            <Typography variant="subtitle2" align="center">
+              Thursday, April 4
+            </Typography>
+          </div>
+          <br /> <br /> <br />
+          <div className={classes.textStyle2}>
+            <Typography variant="subtitle2" align="center">
+              No upcoming events
+            </Typography>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
