9116ea190e3347a20a47d0141e90c436bf224792
Sheryl Seeyave <sherylseeyave@gmail.com>
2021-11-09 01:46:52 -0600

    Vista Settings and Vista Narratives

--- frontend/src/components/Vista/VistaNarratives/index.tsx
+++ frontend/src/components/Vista/VistaNarratives/index.tsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TextField from '../VistaClockIn/textField';
+import DateSelector from '../VistaClockIn/dateSelector';
+import history from '../../../history';
 import styles from './styles.module.css'
 
 
 export default function VistaNarratives(){
+    const [date,setDate]=useState<Date | null>();
+    const [summary,setSummary]=useState<String>();
+
+    const handleSubmit=()=>{
+        alert("Entry Submitted");
+        console.log("date: "+date?.getDate);
+        console.log("quic summary:"+summary);
+        history.goBack();
+    };
     return(
+<<<<<<< Updated upstream
         <div>narr</div>
+=======
+        <div>
+            <div className={styles.topBar}>
+                <h1>Narratives</h1>
+            </div>
+            <hr/>
+
+            <div className={styles.container}>
+                <div className={styles.dateSelector}>
+                    <DateSelector setSelectedDate={setDate}/> 
+                </div>
+
+                <div className={styles.text}>
+                    {/* textbox for daily activity field */}
+                    <TextField setSummary={setSummary}/>
+                    {/* submit button */}
+                    <button className={styles.submitButton} onClick={()=>{handleSubmit()}}>Submit</button>
+                </div>
+            </div>
+        </div>
+>>>>>>> Stashed changes
     );
 }
--- frontend/src/components/Vista/VistaSettings/index.tsx
+++ frontend/src/components/Vista/VistaSettings/index.tsx
@@ -4,6 +4,39 @@ import styles from './styles.module.css'
 
 export default function VistaSettings(){
     return(
+<<<<<<< Updated upstream
         <div>narr</div>
+=======
+        <div>
+            <div className={styles.topBar}>
+                <h1>Settings</h1>
+            </div>
+            <hr/>
+            <div className={styles.container}>
+                <div className={styles.textDiv}>
+                    Name
+                    <input className={styles.textInput} type="text" name="name"/>
+                </div>
+                <div className={styles.textDiv}>
+                    <label>
+                    Company Email
+                    <input className={styles.textInput} type="text" name="companyEmail"/>
+                    </label>
+                </div>
+                <div className={styles.textDiv}>
+                    <label>
+                    Personal Email
+                    <input className={styles.textInput} type="text" name="personalEmail"/>
+                    </label>
+                </div>
+                <div className={styles.textDiv}>
+                    <label>
+                    Password
+                    <input className={styles.textInput} type="text" name="password"/>
+                    </label>
+                </div>
+            </div>
+        </div>
+>>>>>>> Stashed changes
     );
 }
