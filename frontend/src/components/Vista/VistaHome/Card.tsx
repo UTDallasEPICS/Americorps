66c6e15f4602bb1a56c0958057af216748f6f13b
Stephanie Li <listeph@gmail.com>
2021-11-10 15:27:18 -0600

    update 11/10

--- frontend/src/components/Vista/VistaHome/Card.tsx
+++ frontend/src/components/Vista/VistaHome/Card.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-// import styles from './styles.module.css'; take out?
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
