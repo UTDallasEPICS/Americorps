e5e9c0e8a20705e4b3e6f90af82a9a9ff1ef69d5
Sheryl Seeyave <sherylseeyave@gmail.com>
2021-12-08 12:44:00 -0600

    Vista Narrative

--- frontend/src/components/Vista/VistaNarratives/textField.tsx
+++ frontend/src/components/Vista/VistaNarratives/textField.tsx
@@ -20,6 +20,7 @@ export default function TextField(props:any){
                 inputProps={{className:styles.textFiledInput}}
                 InputLabelProps={{className:styles.textFiledInput}} 
                 onChange={handleChange} 
+                fullWidth
                 />
         </div>
     );
