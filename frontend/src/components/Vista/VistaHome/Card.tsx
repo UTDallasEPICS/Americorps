860ed2b7c1e446dfb9f936b2544a3c8fa1c7819b
Stephanie Li <listeph@gmail.com>
2021-12-08 00:03:32 -0600

    stuff

--- frontend/src/components/Vista/VistaHome/Card.tsx
+++ frontend/src/components/Vista/VistaHome/Card.tsx
@@ -1,64 +1,58 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
-    minWidth: 275,
-    backgroundColor:'transparent',
-    borderRadius:'5%',
-    color:'white'
+    "& > *": {
+      margin: theme.spacing(1),
+      width: theme.spacing(30),
+      height: theme.spacing(40)
+    }
   },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-    color:'white'
+  blueCard: {
+    backgroundImage: "linear-gradient(to bottom, #047257 50%, #061532 50%)",
+    borderRadius: 25,
+    maxWidth: 300
   },
-  title: {
-    fontSize: 14,
-    color:'white'
-  },
-  pos: {
-    marginBottom: 12,
-    color:'white'
-  },
-});
 
-export default function SimpleCard() {
+
+  textStyle1: {
+    color: "white",
+    padding: 50,
+    fontWeight: 600
+  },
+  textStyle2: {
+    color: "white"
+  }
+}));
+
+export default function Variants() {
   const classes = useStyles();
+
   return (
-    <Card className={classes.root}>
-      <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Here is your daily briefing:
-        </Typography>
-        <Typography variant="h5" component="h2">
-          12:33pm
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          on call
-        </Typography>
-        <Typography variant="body2" component="p">
-          Complete the task that you had
-          <br />
-          Then proceed with the following task
-          <br />
-          Then you have to do this one more
-          <br />
-          one more task just because
-          <br />
-          uno mas
-          <br />
-        </Typography>
-      </CardContent>
-      <CardActions >
-        <Button className={classes.root} size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+    <div className={classes.root}>
+      <Card className={classes.blueCard}>
+        <CardContent>
+          <div className={classes.textStyle1}>
+            <Typography variant="h5" align="center">
+              12:06 PM
+            </Typography>
+
+            <Typography variant="subtitle2" align="center">
+              Thursday, April 4
+            </Typography>
+          </div>
+          <br /> <br /> <br />
+          <div className={classes.textStyle2}>
+            <Typography variant="subtitle2" align="center">
+              No upcoming events
+            </Typography>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
--- frontend/src/components/Vista/VistaSchedule/index.tsx
+++ frontend/src/components/Vista/VistaSchedule/index.tsx
@@ -1,10 +1,46 @@
 import React from 'react';
 import styles from './styles.module.css'
 
+// Change name to history
 
 export default function VistaSchedule(){
     return(
-        <div className={styles.root}>
+        <div>
+            <div className={styles.topBar}>
+                <h1>History</h1>
+            </div>
+            <hr/>
+            <br/>
+            <div className={styles.container}>
+                <div>
+                   <button>January 2021
+                   </button>
+               </div>
+               <br/>
+               <div>
+                   <button>Feburary 2021</button>
+               </div>
+               <br/>
+               <div>
+                   <button> March 2021 </button>
+               </div>
+               <br/>
+               <div>
+                   <button>April 2021</button>
+               </div>
+               <br/>
+               <div>
+                   <button>May 2021</button>
+               </div>
+               <br/>
+               <div>
+                   <button>June 2021</button>
+               </div>
+               <br/>
+               <div>
+                   <button>July 2021</button>
+               </div>
+            </div>
             
         </div>
     );
